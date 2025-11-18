1d0aee2ef8cade2306137385f541c316447100b9
Flames <vibe-coder@flames.com>
2025-11-18 12:05:44 +0000

    Update project prj_49e6dh9k - 2025-11-18T12:05:43.566Z

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import App from './App'
 import Test from './Test'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/*" element={<App />} />
         <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
